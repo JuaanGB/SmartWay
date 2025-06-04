790409a823afb3ed27af5f97a1e6216b23e92890
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-06-04 13:52:10 +0200

    Menú de navegación en pantalla pequeña
    - Aparece la típica "hamburguesa" para mostrar el menú de navegación en dispositivos pequeños
    - El cambio de tema funciona pero de momento es feo: cambia color de texto, fondo e imagen que representa el modo actual.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // 0: claro, 1: oscuro
 var modo = 0;
 
+// 0: ocultos en modo pequeño, 1: visibles en modo pequeño
+var nav = 0;
+
 function tarjetahover(event) {
     var ps = event.target.getElementsByTagName("p");
     var p = ps[0];
@@ -30,13 +33,35 @@ function anadirnuevo(event) {
     }
 }
 
+// Para personalizar estilos en función de un evento, podemos aplicar/quitar clases a los elementos que
+// deseamos que cambien. Habrá que definir estilos para ese elemento y esa clase concreta en el CSS.
 function cambiarTema(event) {
     if (event.type == "click") {
-        modo = !modo;
-        if (modo == 0) { // Claro
-            event.target.setAttribute("src", "images/modo-dia.png");
-        } else { // Oscuro
+        if (modo == 0) { // Claro -> Oscuro
             event.target.setAttribute("src", "images/modo-noche.png");
+            document.body.className = "dark";
+        } else { // Oscuro -> Claro
+            event.target.setAttribute("src", "images/modo-dia.png");
+            document.body.className = "";
         }
+        modo = !modo; // Cambiamos de modo
+    }
+}
+
+function mostrarNav(event) {
+    if (event.type == "click") {
+        var opciones = Array.from(document.getElementsByTagName("nav")[0].children);
+        if (nav == 0) { // invisibles -> visibles
+            event.target.setAttribute("src", "images/nav-close.png");
+            opciones.forEach( n => {
+                n.classList.add("nav-visible");
+            })
+        } else { // invisibles -> visibles
+            event.target.setAttribute("src", "images/nav.png");
+            opciones.forEach( n => {
+                n.classList.remove("nav-visible");
+            })
+        }
+        nav = !nav;
     }
 }
