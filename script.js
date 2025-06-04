10816c8ae032c6972390e1cfd14a22b762e999a0
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-06-04 11:38:59 +0200

    Javascript simple.
    - Cambiar estilo de tarjetas al pasar el ratón.
    - Flex alineado a la derecha de las páginas.

---
+++ script.js
@@ -0,0 +1,28 @@
+function tarjetahover(event) {
+    var ps = event.target.getElementsByTagName("p");
+    var p = ps[0];
+    if (event.type == "mouseenter")  {
+        p.innerHTML = p.innerHTML + " (Seleccionado) "; // Esto cambia el texto original
+    }
+    if (event.type == "mouseleave")  {
+        p.innerHTML = "Seguimiento en tiempo real de solicitudes entrantes a tu sistema."; // Aquí si hiciese = "" seguirá apareciendo
+        // seleccionado porque modifiqué el texto original.
+    }
+}
+
+function anadirnuevo(event) {
+    if (event.type == "mouseenter")  {
+        // Un borde en el div
+        event.target.style.borderStyle = "solid";
+        let div = document.createElement("div");
+        div.innerHTML = "<h3>Clic aquí para saber más</h3>";
+        div.style.background = "white";
+        event.target.append(div);
+    }
+    if (event.type == "mouseleave")  {
+        event.target.style.borderStyle = "";
+
+        let div = event.target.getElementsByTagName("div")[0];
+        div.remove();
+    }
+}
