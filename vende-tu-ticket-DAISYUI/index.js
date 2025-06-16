fb8acec39e8eb8c7598ceebcb672f6ecf29adac4
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-06-16 12:57:26 +0200

    Algo más de diseño responsivo.
    - Icono de tarjetas de seguridad y confianza en columna en dispositivos pequeños.
    - Icono de qr cambiado a un svg para que cambie junto al tema.
    - La información de las tarjetas aparece ahora al hacer clic -> diseño para móviles (eliminar/añadir clases de visibilidad con JS).
    - Ese evento ahora se añade en el contenedor en el html, y buscamos la tarjeta más cercana (closest)

--- vende-tu-ticket-DAISYUI/index.js
+++ vende-tu-ticket-DAISYUI/index.js
@@ -1,29 +1,17 @@
 function mostrarInformacionPaso(event) {
-    const tarjeta = event.target;
-    const id = tarjeta.id; // Acabará en 1, 2, 3 o 4
-    const num = id.slice(-1);
-    const info = document.getElementById("info-paso"); // Podría simplemente utilizar infoPaso
+    const click = event.target.closest(".card");
 
-    if (event.type == "mouseenter") {
-        switch (num) {
-            case "1":
-                info.textContent = "Regístrate o crea una cuenta nueva en el sistema para comenzar a vender tus tickets.";
-                break;
-            case "2":
-                info.textContent = "Rellena un formulario con información sobre el evento y adjunta la entrada en formato PDF.";
-                break;
-            case "3":
-                info.textContent = "Un administrador validará la información introducida del evento en un plazo de tres días. Si hay algún error, se te notificará por correo electrónico.";
-                break;
-            case "4":
-                info.textContent = "¡Tu entrada ya estará disponible para el resto de usuarios que quieran comprar!";
-                break;
-            default:
-                info.textContent = "Sitúa el ratón sobre un paso para conocer sus detalles.";
-        }
-    } 
-    if (event.type == "mouseleave") {
-        info.textContent = "Sitúa el ratón sobre un paso para conocer sus detalles.";
+    if (!click) return;
+
+    const tarjeta = click.getElementsByTagName("p")[0];
+    
+    if (tarjeta.classList.contains("hidden")) {
+        tarjeta.classList.remove("hidden");
+        tarjeta.classList.add("block");
+    } else {
+        tarjeta.classList.add("hidden");
+        tarjeta.classList.remove("block");
     }
+    
 }
 
