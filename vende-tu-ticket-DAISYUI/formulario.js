c0678fee8aee711e5c2aa7ef64798c6e896cba66
JuaanGB <juanalejandro.gonzalezb@um.es>
2025-06-17 09:42:59 +0200

    Adjuntar la foto en formulario.
    - El input pide una foto. Si se la das, oculta los elementos originales del label y muestra la foto como background.
    - Para eliminarla, aparecerá un botón con una X al hacer hover sobre la imagen.

--- vende-tu-ticket-DAISYUI/formulario.js
+++ vende-tu-ticket-DAISYUI/formulario.js
@@ -1,3 +1,29 @@
 window.onbeforeunload = function() {
     return false;
 };
+
+const input = document.getElementById("foto-evento-input");
+const label = document.getElementById("foto-evento-img");
+const botonX = document.getElementById("boton-foto-evento");
+
+input.addEventListener("change", function (e) {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = function () {
+        label.style.backgroundImage = `url(${reader.result})`;
+        label.style.backgroundSize = "cover";
+        label.style.backgroundPosition = "center";
+        label.classList.add("hay-foto");
+    };
+    reader.readAsDataURL(file);
+});
+
+botonX.addEventListener("click", function (e) {
+    e.preventDefault();
+    input.value = "";
+    label.style.backgroundImage = "";
+    label.classList.remove("hay-foto");
+});
+
